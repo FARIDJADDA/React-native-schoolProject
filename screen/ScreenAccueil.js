553e2e34e854e88178452b671a3b8a104517df02
Karim Damad <karim.damad.p@hotmail.com>
2021-04-08 10:59:10 +0200

    commit front

---
+++ screen/ScreenAccueil.js
@@ -0,0 +1,73 @@
+import React from "react";
+import {
+  View,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  Dimensions,
+  Text} from "react-native";
+import iconMenu from "../assets/icon_menu.png"
+import logoMG from "../assets/logoMG.png"
+
+
+export default function Accueil({ navigation }) {
+    return (
+      <View style={styles.container}>   
+        <View style={styles.rowStart}> 
+          <TouchableOpacity onPress={() => navigation.openDrawer()}>
+            <Image source={iconMenu} style={styles.iconMenu}/>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.rowCenter}>
+          <Image style={styles.imageLogoMG} source={logoMG}/>
+        </View>
+        <View style={styles.rowCenter}>
+          <Text style={styles.textTitre}>MobinGame</Text>
+        </View>
+        <View style={styles.rowCenter}>
+          <Text style={styles.textDescription}>Pour accéder à la liste des joueurs et des jeux, cliquez sur le menu en haut à gauche ou glissez votre doigt de gauche à droite</Text>
+        </View>
+      </View>
+    );
+  }
+
+  // ---------------------------- STYLE ----------------------------
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      display: "flex",
+      backgroundColor: "#7F7F7F"
+    },
+    rowStart:{
+      flexDirection: "row",
+      justifyContent: "flex-start",
+      alignItems: "flex-start",
+      marginBottom: 10
+    },
+    rowCenter:{
+      flexDirection: "row",
+      justifyContent: "center",
+      alignItems: "center",
+      marginBottom: 10
+    },
+    iconMenu:{
+      marginTop: 40,
+      marginLeft: 20
+    },
+    imageLogoMG:{
+      width: Dimensions.get("window").width-250,
+      height:Dimensions.get("window").height-450
+    },
+    textTitre:{
+      fontSize: 25,
+      color: "black",
+      marginTop: -10,
+      marginBottom: 10
+    },
+    textDescription:{
+      fontSize: 18,
+      color: "black",
+      textAlign: "center",
+    }
+  })
---
+++ screen/ScreenListeJoueur.js
@@ -0,0 +1,115 @@
+import React from "react";
+import {
+  View,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  Text,
+  FlatList
+} from "react-native";
+import iconMenu from "../assets/icon_menu.png"
+
+function Item ({ item }) {
+    return(
+      <View style={styles.rowJoueur}>
+        <Image style={styles.imageJoueur} source={{uri:item.image}}/>
+        <Text style={styles.textTitre}>{item.title}</Text>
+      </View>
+    );
+}    
+
+state = {
+  data: [
+    {
+      "id": "0001",
+      "image": "https://firebasestorage.googleapis.com/v0/b/moga-61d86.appspot.com/o/userImage%2FmobingameAvatar1.png?alt=media&token=1a27e5f8-69f8-464c-8d42-1777a720a642",
+      "title": "DMDProd",
+    },
+    {
+      "id": "0002",
+      "image": "https://firebasestorage.googleapis.com/v0/b/moga-61d86.appspot.com/o/userImage%2FmobingameAvatar1.png?alt=media&token=1a27e5f8-69f8-464c-8d42-1777a720a642",
+      "title": "Farid",
+    },
+    {
+      "id": "0003",
+      "image": "https://firebasestorage.googleapis.com/v0/b/moga-61d86.appspot.com/o/userImage%2FmobingameAvatar1.png?alt=media&token=1a27e5f8-69f8-464c-8d42-1777a720a642",
+      "title": "Rokenson",
+    },
+    {
+      "id": "0004",
+      "image": "https://firebasestorage.googleapis.com/v0/b/moga-61d86.appspot.com/o/userImage%2FmobingameAvatar1.png?alt=media&token=1a27e5f8-69f8-464c-8d42-1777a720a642",
+      "title": "Tiffany",
+    },
+    {
+      "id": "0005",
+      "image": "https://firebasestorage.googleapis.com/v0/b/moga-61d86.appspot.com/o/userImage%2FmobingameAvatar1.png?alt=media&token=1a27e5f8-69f8-464c-8d42-1777a720a642",
+      "title": "Tiffany",
+    },
+    {
+      "id": "0006",
+      "image": "https://firebasestorage.googleapis.com/v0/b/moga-61d86.appspot.com/o/userImage%2FmobingameAvatar1.png?alt=media&token=1a27e5f8-69f8-464c-8d42-1777a720a642",
+      "title": "Tiffany",
+    },
+  ]
+}
+
+  export default function ListeJoueur({ navigation }) {
+    const renderItem = ({ item }) => (<Item item={item} />
+    );
+    
+    return (
+      <View style={styles.container}>   
+        <View style={styles.rowStart}> 
+          <TouchableOpacity onPress={() => navigation.openDrawer()}>
+            <Image source={iconMenu} style={styles.iconMenu} />
+          </TouchableOpacity>
+        </View>
+          <FlatList
+            data={state.data}
+            renderItem={renderItem}
+            keyExtractor={item => item.id}
+          />
+      </View>
+    );
+  }
+
+  
+  // ---------------------------- STYLE ----------------------------
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      display: "flex",
+      backgroundColor: "#7F7F7F"
+    },
+    rowStart:{
+      flexDirection: "row",
+      justifyContent: "flex-start",
+      alignItems: "flex-start",
+      marginBottom: 10
+    },
+    rowJoueur:{
+      borderWidth:2,
+      flexDirection: "row",
+      alignItems: "center",
+      marginBottom: 10,
+      borderRadius: 20,
+      backgroundColor: "#CECECE"
+    },
+    iconMenu:{
+      marginTop: 40,
+      marginLeft: 20
+    },
+    imageJoueur:{
+      height: 100,
+      width: 100,
+      marginLeft: 20,
+      marginRight: 20
+    },
+    textTitre:{
+      fontSize: 25,
+      color: "black",
+      marginTop: -10,
+      marginBottom: 10
+    },
+  })
